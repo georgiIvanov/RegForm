c3fb2c633c769f52bb706761318bb8e0e7e0e855
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-07-03 21:48:34 +0300

    added navigation and login view controllers

---
+++ TestYL/ViewControllers/NavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.h
+//  TestYL
+//
+//  Created by Georgi Ivanov on 7/3/14.
+//  Copyright (c) 2014 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationViewController : UINavigationController
+
+@end
