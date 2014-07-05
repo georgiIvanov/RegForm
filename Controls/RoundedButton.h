9970218348cb51b55f5729b2ed1d3913ead1dffd
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-07-05 23:56:15 +0300

    added RoundedButton control

---
+++ Controls/RoundedButton.h
@@ -0,0 +1,13 @@
+//
+//  RoundedButton.h
+//  TestYL
+//
+//  Created by Georgi Ivanov on 7/5/14.
+//  Copyright (c) 2014 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundedButton : UIButton
+
+@end
---
+++ Controls/RoundedButton.m
@@ -0,0 +1,67 @@
+//
+//  RoundedButton.m
+//  TestYL
+//
+//  Created by Georgi Ivanov on 7/5/14.
+//  Copyright (c) 2014 YL. All rights reserved.
+//
+
+#import "RoundedButton.h"
+#import <POP.h>
+
+@interface RoundedButton()
+
+- (void)setup;
+- (void)scaleToSmall;
+- (void)scaleToFullSize;
+- (void)scaleToDefault;
+
+@end
+
+@implementation RoundedButton
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    [self addTarget:self action:@selector(scaleToSmall)
+   forControlEvents:UIControlEventTouchDown | UIControlEventTouchDragEnter];
+    [self addTarget:self action:@selector(scaleToFullSize)
+   forControlEvents:UIControlEventTouchUpInside];
+    [self addTarget:self action:@selector(scaleToDefault)
+   forControlEvents:UIControlEventTouchDragExit];
+}
+
+- (void)scaleToSmall
+{
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(0.93f, 0.95f)];
+    scaleAnimation.duration = 0.1f;
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleSmallAnimation"];
+}
+
+- (void)scaleToFullSize
+{
+    POPSpringAnimation *scaleAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.velocity = [NSValue valueWithCGSize:CGSizeMake(3.f, 3.f)];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.f, 1.f)];
+    scaleAnimation.springBounciness = 8.0f;
+    scaleAnimation.springSpeed = 15;
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleSpringAnimation"];
+}
+
+- (void)scaleToDefault
+{
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.f, 1.f)];
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleDefaultAnimation"];
+}
+
+@end
