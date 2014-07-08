c71f07b9b97c547b6c61f4aa821c4bf037ff6946
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-07-08 20:49:20 +0300

    added mock account service

---
+++ Network/AccountService.h
@@ -0,0 +1,28 @@
+//
+//  AccountService.h
+//  TestYL
+//
+//  Created by Georgi Ivanov on 7/8/14.
+//  Copyright (c) 2014 YL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "UserAccount.h"
+
+
+@protocol AccountService <NSObject>
+
+-(void)loginUser:(UserAccount*)user onSuccess:(void (^)(UserAccount* user))onSuccess onFailure:(void (^)(UserAccount* user, NSDictionary* error))onError;
+
+-(void)registerUser:(UserAccount*)user onSuccess:(void (^)(UserAccount* user))onSuccess onFailure:(void (^)(UserAccount* user, NSDictionary* error))onError;
+
+-(void)resetPassword:(NSString*)email onSuccess:(void (^)(NSDictionary* info))onSuccess onFailure:(void (^)(NSDictionary* error))onError;
+
+@end
+
+// This is only a mock service
+@interface AccountService : NSObject <AccountService>
+
+
+
+@end
---
+++ Network/AccountService.m
@@ -0,0 +1,66 @@
+//
+//  AccountService.m
+//  TestYL
+//
+//  Created by Georgi Ivanov on 7/8/14.
+//  Copyright (c) 2014 YL. All rights reserved.
+//
+
+#import "AccountService.h"
+
+static NSString* const kEmailKey = @"email";
+static NSString* const kPasswordKey = @"password";
+static NSString* const kNameKey = @"name";
+static NSString* const kBirthDatelKey = @"birthDate";
+static NSString* const kAvatarKey = @"avatar";
+static NSString* const kGenderKey = @"gender";
+
+@interface AccountService()
+
+@property(nonatomic, strong) NSUserDefaults* userDefaults;
+
+@end
+
+@implementation AccountService
+
+-(id)init
+{
+    self = [super init];
+    if(self)
+    {
+        self.userDefaults = [NSUserDefaults standardUserDefaults];
+    }
+    return self;
+}
+
+-(void)loginUser:(UserAccount*)user onSuccess:(void (^)(UserAccount* user))onSuccess onFailure:(void (^)(UserAccount* user, NSDictionary* error))onError
+{
+    
+}
+
+-(void)registerUser:(UserAccount*)user onSuccess:(void (^)(UserAccount* user))onSuccess onFailure:(void (^)(UserAccount* user, NSDictionary* error))onError
+{
+    NSDictionary* userDict = @{kEmailKey: user.email,
+                               kPasswordKey: user.password,
+                               kNameKey: user.name,
+                               kBirthDatelKey: user.birthDate,
+                               kAvatarKey: user.avatar,
+                               kGenderKey: @(user.gender)};
+    
+    [self.userDefaults setObject:userDict forKey:user.email];
+    if(![self.userDefaults synchronize])
+    {
+        onError(user, @{@"error": @"Can't persist user"});
+    }
+    else
+    {
+        onSuccess(user);
+    }
+}
+
+-(void)resetPassword:(NSString*)email onSuccess:(void (^)(NSDictionary* info))onSuccess onFailure:(void (^)(NSDictionary* error))onError
+{
+    
+}
+
+@end
